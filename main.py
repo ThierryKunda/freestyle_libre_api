f7580e98d2bc678010b985e8b3cff6de65d0952a
thierry.kunda <thierry.kunda@universite-paris-saclay.fr>
2023-02-15 16:56:46 +0100

    First get uris

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
 from typing import Union
+from datetime import datetime
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+import models
+
+import api
 
 app = FastAPI()
 
@@ -14,10 +20,15 @@ class Item(BaseModel):
 def read_root():
     return {"Hello", "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+samples = api.samples_from_csv(filepath=os.path.join("tests","glucose_real_data.csv"))
+stats = models.Stats.from_sample_collection(samples)
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/samples")
+def read_samples(day: Union[str, None] = None):
+    if day is None:
+        return list(filter(lambda d: d.sampling_date.date() == datetime.today().date(), samples))
+    return list(filter(lambda d: datetime.strptime(day, "%d-%m-%Y").date() == d.sampling_date.date(), samples))
+
+@app.get("/stats")
+def read_stats():
+    return stats
