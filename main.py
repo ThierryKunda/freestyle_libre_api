4426602671a5aa9e85881fc3ff46c21f8130751d
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-02-16 21:30:15 +0100

    Endpoints for hours and days intervals

--- main.py
+++ main.py
@@ -27,3 +27,15 @@ def read_samples(day: Union[str, None] = None):
 @app.get("/stats")
 def read_stats():
     return stats
+
+@app.get("/trend/hours_interval")
+def read_trend_hours(h1_string: str, h2_string: str, error: int):
+    h1 = datetime.strptime(h1_string, "%d-%m-%Y_%H:%M")
+    h2 = datetime.strptime(h2_string, "%d-%m-%Y_%H:%M")
+    return models.HourTrend.from_hours(h1,h2,samples, error)
+
+@app.get("/trend/days_interval")
+def read_trend_days(day1_string: str, day2_string: str, error: int):
+    day1 = datetime.strptime(day1_string, "%d-%m-%Y")
+    day2 = datetime.strptime(day2_string, "%d-%m-%Y")
+    return models.HourTrend.from_hours(day1,day2,samples, error)
