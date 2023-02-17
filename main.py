908032dc76da565cf7bbd76ee84e3f1fe5c3105b
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-02-17 19:29:41 +0100

    Changed {username} place in the endpoint

--- main.py
+++ main.py
@@ -30,18 +30,18 @@ def read_samples(username: str, day: Optional[str] = None):
 def read_stats(username: str):
     return stats[username]
 
-@app.get("/trend/{username}/hours_interval")
+@app.get("/user/{username}/trend/hours_interval")
 def read_trend_hours(username: str, h1_string: str, h2_string: str, error: int):
     h1 = datetime.strptime(h1_string, "%d-%m-%Y_%H:%M")
     h2 = datetime.strptime(h2_string, "%d-%m-%Y_%H:%M")
     return models.HourTrend.from_hours(h1,h2,samples[username], error)
 
-@app.get("/trend/{username}/days_interval")
+@app.get("/user/{username}/trend/days_interval")
 def read_trend_days(username: str, day1_string: str, day2_string: str, error: int):
     day1 = datetime.strptime(day1_string, "%d-%m-%Y")
     day2 = datetime.strptime(day2_string, "%d-%m-%Y")
     return models.HourTrend.from_hours(day1,day2,samples[username], error)
 
-@app.get("/trend/{username}/months_interval")
+@app.get("/user/{username}/trend/months_interval")
 def read_trend_months(username: str, month1: int, year1: int, month2: int, year2: int, error: int):
     return models.MonthTrend.from_months(month1, year1, month2, year2, samples[username], error)
