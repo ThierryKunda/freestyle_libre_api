4d77b6df970213ade0b6b234f856975aa9563bbb
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-04-19 18:04:45 +0200

    Added add_new_token function

--- utils.py
+++ utils.py
@@ -16,6 +16,27 @@ def add_new_user(db: Session, firstname: str, lastname: str, password: str):
     db.commit()
     return user
 
+def add_new_token(
+        db: Session, firstname: str, lastname: str, password: str,
+        user_profile_access: bool, samples_access: bool, goals_access: bool
+        ) -> str:
+    # Get the user
+    pw = encode_secret(password)
+    user = db.query(db_models.User).filter_by(firstname=firstname, lastname=lastname, password=pw).first()
+    if not user:
+        return None
+    # Generate new token based on current date (timestamp)
+    current_dt = dt.now().timestamp()
+    tk_value = encode_secret(current_dt)
+    tk = db_models.Auth(
+        user_id=user.id, token_value=tk_value, expiration_date=dt.now(),
+        last_time_used=dt.now(),
+        user_profile_access=user_profile_access,
+        samples_access = samples_access,
+        goals_access = goals_access
+    )
+    return tk
+
 def get_user_from_token(db: Session, token: str):
     # Checks if token already exists
     tk = db.query(db_models.Auth).filter_by(token_value=token).first()
