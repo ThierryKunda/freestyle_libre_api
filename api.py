42e0956885a404087c46ff392bc2b0db5052c603
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-05-25 20:31:54 +0200

    Using pandas to load data

--- api.py
+++ api.py
@@ -1,8 +1,6 @@
 from enum import Enum
 from datetime import datetime
-import csv
-
-import requests as req
+import pandas as pd
 
 import os
 from models.resources import BloodGlucoseSample, Stats
@@ -14,23 +12,13 @@ class SourceType(Enum):
 def samples_from_csv(data_from: str = SourceType.CSVfile, **query_parameters) -> list[BloodGlucoseSample]:
     res: list[BloodGlucoseSample] = []
     if data_from == SourceType.CSVfile:
-        f = open(os.path.join(os.getcwd(), query_parameters['filepath']), newline='')
-        reader = csv.reader(f, delimiter=',')
-        # Ignore the headers
-        next(reader)
-        next(reader)
-        # Creating samples from each row
-        for row in reader:
-            if row[4] == "-1" or row[4] == "":
-                continue
-            res.append(BloodGlucoseSample(
-                device_name=row[0],
-                device_serial_number=row[1],
-                sampling_date=datetime.strptime(row[2], "%d-%m-%Y %H:%M"),
-                recording_type=row[3],
-                value=int(row[4])
-            ))
-        f.close()
+        df = pd.read_csv("./users_data/ThierryKunda_glucose_11-5-2023.csv", sep=',', header=1, parse_dates=[2], date_format="%d-%m-%Y %H:%M")
+        glucose_samples = df.iloc[:, :5].dropna()
+        glucose_samples.sort_values(inplace=True, by="Horodatage de l'appareil")
+        return [
+            BloodGlucoseSample(device_name=s[0], device_serial_number=s[1], sampling_date=s[2].to_pydatetime(), recording_type=s[3], value=s[4])
+            for s in glucose_samples.values.tolist()
+        ]
     elif data_from == SourceType.sourceUri:
         pass
     else:
@@ -40,7 +28,8 @@ def samples_from_csv(data_from: str = SourceType.CSVfile, **query_parameters) ->
     return res
 
 if __name__ == '__main__':
-    my_data = samples_from_csv(SourceType.CSVfile, filepath=os.path.join("users_data","Benoit_Delarue_03-03-2023.csv"))
+    # v = DataItemValidator(5, 19, True, [["", "-1"] if i == 4 else [] for i in range(20)])
+    my_data = samples_from_csv(SourceType.CSVfile, filepath=os.path.join("users_data","Gerard_Depardieu_02-04-2023.csv"))
     print(my_data, "\n")
     my_stats = Stats.from_sample_collection(my_data)
     print(my_stats)
