1aaab7427a2bc244a86fd0d6e3a2de7286c42122
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-05-26 23:45:45 +0200

    Validate user data

--- api.py
+++ api.py
@@ -1,18 +1,27 @@
+import os
+
 from enum import Enum
 from datetime import datetime
 import pandas as pd
+from pandera.errors import SchemaError
 
-import os
 from models.resources import BloodGlucoseSample, Stats
-
+from data_validation import convert_insulin, user_data_schema
 class SourceType(Enum):
     CSVfile = 'CSVfile'
     sourceUri = 'sourceUri'
 
-def samples_from_csv(data_from: str = SourceType.CSVfile, **query_parameters) -> list[BloodGlucoseSample]:
+def samples_from_csv(data_from: str = SourceType.CSVfile, **query_parameters) -> list[BloodGlucoseSample] | None:
     res: list[BloodGlucoseSample] = []
     if data_from == SourceType.CSVfile:
-        df = pd.read_csv("./users_data/ThierryKunda_glucose_11-5-2023.csv", sep=',', header=1, parse_dates=[2], date_format="%d-%m-%Y %H:%M")
+        df = pd.read_csv(query_parameters["filepath"], sep=',', header=1, parse_dates=[2], date_format="%d-%m-%Y %H:%M", low_memory=False, converters={
+        "Insuline à action longue (unités)": convert_insulin,
+        "Insuline à action rapide (unités)": convert_insulin,
+        })
+        try:
+            user_data_schema.validate(df)
+        except SchemaError:
+            return None
         glucose_samples = df.iloc[:, :5].dropna()
         glucose_samples.sort_values(inplace=True, by="Horodatage de l'appareil")
         return [
@@ -30,6 +39,5 @@ def samples_from_csv(data_from: str = SourceType.CSVfile, **query_parameters) ->
 if __name__ == '__main__':
     # v = DataItemValidator(5, 19, True, [["", "-1"] if i == 4 else [] for i in range(20)])
     my_data = samples_from_csv(SourceType.CSVfile, filepath=os.path.join("users_data","Gerard_Depardieu_02-04-2023.csv"))
-    print(my_data, "\n")
     my_stats = Stats.from_sample_collection(my_data)
     print(my_stats)
--- data_validation.py
+++ data_validation.py
@@ -1,3 +1,8 @@
+import numpy as np
+import pandas as pd
+import pandera as pa
+from pandera.typing import Series, Index, DataFrame
+
 class InvalidDataException(Exception):
     """Exception class for invalid data :
     - invalid syntax
@@ -6,72 +11,44 @@ class InvalidDataException(Exception):
     def __init__(self, message) -> None:
         super().__init__(message)
 
-class DataItemValidator:
-    def __init__(
-            self, min_columns_amount: int, max_columns_amount: int,
-            raise_exception: bool, none_evaluated_values: list[list[str]],
-            data: list[str] | None = None, separator: str = ",", contains_void: list[str] | None = None
-        ) -> None:
-        self.data = data
-        self.raise_exception = raise_exception
-        self.contains_void = contains_void
-        self.min_columns_amount = min_columns_amount
-        self.max_columns_amount = max_columns_amount
-        self.none_evaluated_values = none_evaluated_values
-        self.separator = separator
+def positive_value(v):
+    return v >= 0
 
-    def check_data_initialized(self):
-        if self.data is None:
-            raise ValueError("Data item is not initialized")
+user_data_schema = pa.DataFrameSchema({
+    "Appareil": pa.Column(str),
+    "Numéro de série": pa.Column(str),
+    "Horodatage de l'appareil": pa.Column(str),
+    "Type d'enregistrement": pa.Column(int),
+    "Historique de la glycémie mg/dL": pa.Column(float, pa.Check(positive_value), nullable=True),
+    "Numérisation de la glycémie mg/dL": pa.Column(float, pa.Check(positive_value), nullable=True),
+    "Insuline à action rapide sans valeur numérique": pa.Column(str, nullable=True),
+    "Insuline à action rapide (unités)": pa.Column(float, nullable=True),
+    "Alimentation sans valeur numérique": pa.Column(str, nullable=True),
+    "Glucides (grammes)": pa.Column(float, pa.Check(positive_value), nullable=True),
+    "Glucides (portions)": pa.Column(float, pa.Check(positive_value), nullable=True),
+    "Insuline à action longue sans valeur numérique": pa.Column(str, nullable=True),
+    "Insuline à action longue (unités)": pa.Column(float, nullable=True),
+    "Remarques": pa.Column(str, nullable=True),
+    "Glycémie par bandelette mg/dL": pa.Column(float, pa.Check(positive_value), nullable=True),
+    "Cétone mmol/L": pa.Column(float, nullable=True),
+    "Insuline repas (unités)": pa.Column(float, nullable=True),
+    "Correction insuline (unités)": pa.Column(float, coerce=True, nullable=True),
+    "Insuline modifiée par l'utilisateur (unités)": pa.Column(float, nullable=True)
+},
+coerce=True,
+strict=True)
 
-    def __getitem__(self, key) -> str | None:
-        self.check_data_initialized()
-        if self.none_evaluated_values[key]:
-            if not self.data[key] in self.none_evaluated_values[key]:
-                return self.data[key]
-        return None
-    
-    def size_is_valid(self) -> bool | None:
-        self.check_data_initialized()
-        data_size = len(self.data)
-        if not self.min_columns_amount <= data_size:
-            if self.raise_exception:
-                raise InvalidDataException(
-                "The amount of values does not match the minimum required : {} actual columns < {} minimum columns"
-                .format(data_size, self.min_columns_amount)
-            )
-            else:
-                return False
-        if not data_size <= self.max_columns_amount:
-            if self.raise_exception:
-                raise InvalidDataException(
-                    "The amount of values does not match the maximum required : {} actual columns > {} maximum columns"
-                    .format(data_size, self.max_columns_amount)
-                )
-            else:
-                return False
-        return True
-    
-    def void_is_valid(self) -> bool | None:
-        if self.contains_void:
-            for i in range(len(self.data)):
-                if self.data[i] in self.contains_void[i]:
-                    if self.raise_exception:
-                        raise ValueError("Some data piece are missing")
-                    else:
-                        return False
-        return True
-    
-    def set_data(self, new_data: list[str], verbose: bool):
-        if verbose:
-            print("Data :")
-            print(*new_data, sep= " | ", end="\n")
-        self.data = new_data
+def convert_insulin(value: str) -> np.float64:
+        if value == "":
+            return np.nan
+        v_formatted = value.replace(",", ".")
+        return np.float64(float(v_formatted))
 
-    def exception_is_raised(self):
-        return self.raise_exception
-    
-    def data_is_valid(self) -> bool | None:
-        size_v = self.size_is_valid()
-        void_v = self.void_is_valid()
-        return size_v and void_v
+
+if __name__ == "__main__":
+    df = pd.read_csv("./tests/glucose_real_data.csv", header=0, low_memory=False, converters={
+        "Insuline à action longue (unités)": convert_insulin,
+        "Insuline à action rapide (unités)": convert_insulin,
+        }
+    )
+    user_data_schema.validate(df)
