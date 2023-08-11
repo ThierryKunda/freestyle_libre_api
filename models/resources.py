7f64cdd567fa67d475a065579774dff34e076a96
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-08-11 23:41:24 +0200

    Added AverageDaySample model

--- models/resources.py
+++ models/resources.py
@@ -42,6 +42,10 @@ class BloodGlucoseSample(BaseModel):
     def __repr__(self) -> str:
         return repr(self.value) + "   " + repr(self.sampling_date) + "   " + self.device_name
 
+class AverageDaySample(BaseModel):
+    hour: time
+    average_value: int
+
 class UpdatedKey(Enum):
     title = 'title'
     status = 'status'
