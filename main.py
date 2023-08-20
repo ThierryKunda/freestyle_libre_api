b3124cbb92560f082f7dc8fdb7e28eaf3bec0a37
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-08-20 13:17:20 +0200

    Replaced declared imports with router_import content

--- main.py
+++ main.py
@@ -1,39 +1,11 @@
-import os
-from typing import Optional
-from datetime import datetime
-
-from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends, Security, status
-from fastapi.responses import FileResponse, HTMLResponse
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, SecurityScopes
-from fastapi.middleware.cors import CORSMiddleware
-
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy import create_engine
-from data_validation import validate_data_from_upload
-
-from models import resources
-from models.database import Base, User
-
-import api, utils
-FRONT_END_APP_URI = os.environ['FRONT_END_APP_URI']
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base.metadata.create_all(bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
+from routers.router_imports import *
+import env
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONT_END_APP_URI],
+    allow_origins=[env.FRONT_END_APP_URI],
     allow_origin_regex=r'https?://(localhost|127\.0\.0\.1).*',
     allow_credentials=True,
     allow_methods=['*'],
