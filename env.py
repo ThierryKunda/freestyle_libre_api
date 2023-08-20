55d2162fb778e89753cbd1056036450f4d999b0a
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-08-20 13:18:32 +0200

    Catch exception for FLAPI_ENV env var

--- env.py
+++ env.py
@@ -3,7 +3,10 @@ from typing import Literal
 
 FRONT_END_APP_URI = os.environ['FRONT_END_APP_URI']
 try:
-    SQLALCHEMY_DATABASE_URL: Literal['DEV', 'PROD'] = os.environ['DB_URL']
+    SQLALCHEMY_DATABASE_URL = os.environ['DB_URL']
 except KeyError:
     SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite"
-ENVIRONNEMENT = os.environ['FLAPI_ENV']
+try:
+    ENVIRONNEMENT: Literal['DEV', 'PROD'] = os.environ['FLAPI_ENV']
+except KeyError:
+    ENVIRONNEMENT = 'DEV'
