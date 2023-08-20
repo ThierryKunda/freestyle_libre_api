4f5fe1b112c2fbdf5dccb36fe649ec0111b342bc
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-08-20 16:40:40 +0200

    Removed user prefix to trend routes

--- routers/user/trend.py
+++ routers/user/trend.py
@@ -4,7 +4,7 @@ from router_dependencies import *
 
 router = APIRouter(tags=["Trends"])
 
-@router.get("/user/{username}/trend/hours_interval")
+@router.get("/{username}/trend/hours_interval")
 def read_trend_hours(username: str, h1_string: str, h2_string: str, error: int, user: User = Security(get_authorized_user, scopes=['samples'])):
     check_username(username, user)
     lazy_load_user_data(username)
@@ -12,7 +12,7 @@ def read_trend_hours(username: str, h1_string: str, h2_string: str, error: int, 
     h2 = datetime.strptime(h2_string, "%d/%m/%Y-%H:%M")
     return resources.HourTrend.from_hours(h1,h2,samples_collection[username], error)
 
-@router.get("/user/{username}/trend/days_interval")
+@router.get("/{username}/trend/days_interval")
 def read_trend_days(username: str, day1_string: str, day2_string: str, error: int, user: User = Security(get_authorized_user, scopes=['samples'])):
     check_username(username)
     lazy_load_user_data(username)
@@ -21,7 +21,7 @@ def read_trend_days(username: str, day1_string: str, day2_string: str, error: in
     day2 = datetime.strptime(day2_string, "%d/%m/%Y")
     return resources.HourTrend.from_hours(day1,day2,samples_collection[username], error)
 
-@router.get("/user/{username}/trend/months_interval")
+@router.get("/{username}/trend/months_interval")
 def read_trend_months(username: str, month1: int, year1: int, month2: int, year2: int, error: int, user: User = Security(get_authorized_user, scopes=['samples'])):
     check_username(username, user)
     lazy_load_user_data(username)
