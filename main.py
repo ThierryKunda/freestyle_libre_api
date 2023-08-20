aaf61df85b03b9a3003a99bab844ab577e826b26
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-08-20 14:20:49 +0200

    Removed app routes for "/user"

--- main.py
+++ main.py
@@ -141,17 +141,6 @@ async def read_root():
     return HTMLResponse(content=content)
 
 
-@app.post("/user")
-async def new_user(user: resources.CreateUser, db: Session = Depends(get_db)):
-    new_user = utils.add_new_user(db, user.firstname, user.lastname, user.password)
-    if new_user:
-        tk = utils.add_new_token(db, user.firstname, user.lastname, user.password, True, True, True, 1, "days")
-        return tk
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="User already exists"
-        )
 
 @app.get("/new_account")
 async def create_new_account():
@@ -301,22 +290,7 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = 
         )
     return resources.Token(access_token=tk["value"], token_type="bearer")
 
-@app.get("/user")
-async def get_user_infos(user: User = Security(get_authorized_user, scopes=['profile'])):
-    return resources.User(
-        user_id=user.id,
-        firstname=user.firstname,
-        lastname=user.lastname,
-        username=user.firstname+"_"+user.lastname,
-        email=user.email,
-        devices_list=utils.get_user_devices(user)
-    )
 
-@app.delete("/user")
-async def remove_user_account(db: Session = Depends(get_db), user: User = Security(get_authorized_user, scopes=['profile', 'samples', 'goals'])):
-    all_info = utils.remove_user(db, user)
-    print("User deleted")
-    return all_info
 
 @app.get("/user/{username}/raw_data")
 async def get_user_data_file(username: str, user: User = Security(get_authorized_user, scopes=['samples'])):
