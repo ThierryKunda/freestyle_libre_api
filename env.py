834d2d338f16e26896be111c7db5aca27207a5c8
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2023-09-02 15:50:34 +0200

    Added port env value

--- env.py
+++ env.py
@@ -1,6 +1,7 @@
 import os
 from typing import Literal
 
+PORT = os.getenv('PORT', "8000")
 SQLALCHEMY_DATABASE_URL = os.getenv('DB_URL', "sqlite:///./db.sqlite")
 FRONT_END_APP_URI = os.getenv('FRONT_END_APP_URI', "http://localhost:3000")
 ENVIRONNEMENT: Literal['DEV', 'PROD'] = os.getenv('FLAPI_ENV', 'DEV')
